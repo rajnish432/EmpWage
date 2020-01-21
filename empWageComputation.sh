ae81a57b8f2793e5010a4a1811d7d055f9f894e5
admin <rajnish.kahar1996@gmail.com>
2020-01-21 16:55:45 +0530

    ADDED: Monthly Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,22 +1,24 @@
-#! /bin/bash 
+#! /bin/bash -x 
 
 echo "Welcome to Employee wage";
 WAGE_PER_HOUR=20;
 FULL_DAY_HOUR=8;
 PART_TIME_HOUR=4;
+WORKING_DAY_PER_MONTH=20;
 
-attendence=$((RANDOM%2))
-if [ $attendence -eq 1 ]
-then
-	echo "Employee present"
-	case $((RANDOM%2)) in
-		0)
-		dailyWage=$((WAGE_PER_HOUR*FULL_DAY_HOUR));;
-		1)
-		dailyWage=$((WAGE_PER_HOUR*PART_TIME_HOUR));;
-	esac
-else
-	echo "Employee absent"
-	dailyWage=0;
-fi
+for (( i=0; i<$WORKING_DAY_PER_MONTH; i++ ))
+do
+	attendence=$((RANDOM%2))
+	if [ $attendence -eq 1 ]
+	then
+			case $((RANDOM%2)) in
+				0)
+					dailyWage=$((dailyWage+(WAGE_PER_HOUR*FULL_DAY_HOUR)));;
+				1)
+					dailyWage=$((dailyWage+(WAGE_PER_HOUR*PART_TIME_HOUR)));;
+			esac
+	else
+				((dailyWage+=0));
+	fi
+done
 	echo "Employee wage: "$dailyWage
