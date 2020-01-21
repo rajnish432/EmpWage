57efa8378890933b7610829ba9cffa4b899cc3ee
admin <rajnish.kahar1996@gmail.com>
2020-01-21 19:21:29 +0530

    REFACTORED:Work Hours using Function

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -9,22 +9,32 @@ actual_working_day=0;
 month_Hrs=0;
 WORKING_HOUR_PER_MONTH=100;
 
+function getWorkingHrs()
+{		monthHrs=$1;
+		case $((RANDOM%2)) in
+            0)
+               ((monthHrs+=8))
+	              ;;
+            1)
+               ((monthHrs+=4))
+   	           ;;
+		esac
+	echo "$monthHrs"
+}
+
 while [[ $month_Hrs -le $WORKING_HOUR_PER_MONTH && $actual_working_day -ne $WORKING_DAY_PER_MONTH ]]
 do
 	attendence=$((RANDOM%2))
 	if [ $attendence -eq 1 ]
 	then
 			((actual_working_day++))
-			case $((RANDOM%2)) in
-				0)
-					((month_Hrs+=8))
-					dailyWage=$((dailyWage+(WAGE_PER_HOUR*FULL_DAY_HOUR)));;
-				1)
-					((month_Hrs+=4))
-					dailyWage=$((dailyWage+(WAGE_PER_HOUR*PART_TIME_HOUR)));;
-			esac
+			temp=$month_Hrs;
+			res="$( getWorkingHrs $temp )";
+			month_Hrs=$res;
+			dailyWage=$((res*$WAGE_PER_HOUR));
 	else
 			((dailyWage+=0));
 	fi
 done
 	echo "Employee wage: "$dailyWage
+	echo "Employee Working hrs: "$res
