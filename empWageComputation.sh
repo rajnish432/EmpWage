32f0d9a1664210284ffd7acc3e15141f8e05a44a
admin <rajnish.kahar1996@gmail.com>
2020-01-21 15:03:01 +0530

    ADDED: Daily Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,11 +1,16 @@
 #! /bin/bash 
 
 echo "Welcome to Employee wage";
-
+WAGE_PER_HOUR=20;
+FULL_DAY_HOUR=8;
 attendence=$((RANDOM%2))
 if [ $attendence -eq 1 ]
 then
+	dailyWage=$((WAGE_PER_HOUR*FULL_DAY_HOUR))
+	echo "Employee wage: "$dailyWage
 	echo "Employee present"
 else
 	echo "Employee absent"
+	dailyWage=0;
+	echo "Employee wage: "$dailyWage
 fi
