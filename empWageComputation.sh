352b268674a9e5a5b44c3d568f0ff1329073ac7c
admin <rajnish.kahar1996@gmail.com>
2020-01-21 16:01:43 +0530

    ADDED: Part Time Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,14 +3,20 @@
 echo "Welcome to Employee wage";
 WAGE_PER_HOUR=20;
 FULL_DAY_HOUR=8;
+PART_TIME_HOUR=4;
+
 attendence=$((RANDOM%2))
 if [ $attendence -eq 1 ]
 then
-	dailyWage=$((WAGE_PER_HOUR*FULL_DAY_HOUR))
-	echo "Employee wage: "$dailyWage
 	echo "Employee present"
+	if [ $((RANDOM%2)) -eq 1 ]
+	then
+		dailyWage=$((WAGE_PER_HOUR*FULL_DAY_HOUR))
+	else
+		dailyWage=$((WAGE_PER_HOUR*PART_TIME_HOUR))
+	fi
 else
 	echo "Employee absent"
 	dailyWage=0;
-	echo "Employee wage: "$dailyWage
 fi
+	echo "Employee wage: "$dailyWage
